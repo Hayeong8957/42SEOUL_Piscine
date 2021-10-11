54a5923370f317d156e4ed637f91b695508f743b
Hayeong Shin <hashin@c5r2s1.42seoul.kr>
2021-10-11 11:24:58 +0900

    C00_ex05

---
+++ C00/ex05/ft_print_comb.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hashin <hashin@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/11 10:22:56 by hashin            #+#    #+#             */
+/*   Updated: 2021/10/11 11:24:25 by hashin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_int(char a, char b, char c);
+
+void	ft_print_comb(void)
+{
+	char	first;
+	char	second;
+	char	third;
+
+	first = '0';
+	while (first <= '7')
+	{
+		second = first + 1;
+		while (second <= '8')
+		{
+			third = second + 1;
+			while (third <= '9')
+			{
+				ft_print_int(first, second, third);
+				if (first != '7' || second != '8' || third != '9')
+				{
+					write(1, ",", 1);
+					write(1, " ", 1);
+				}
+				third++;
+			}
+			second++;
+		}
+		first++;
+	}
+}
+
+void	ft_print_int(char a, char b, char c)
+{
+	write(1, &a, 1);
+	write(1, &b, 1);
+	write(1, &c, 1);
+}
