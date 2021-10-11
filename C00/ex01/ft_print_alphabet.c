881b4bd82edd4ccee87c55b03b9702b3c7895fef
Hayeong Shin <hashin@c5r2s1.42seoul.kr>
2021-10-11 09:57:53 +0900

    C00_ex01

---
+++ C00/ex01/ft_print_alphabet.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hashin <hashin@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/11 09:52:51 by hashin            #+#    #+#             */
+/*   Updated: 2021/10/11 09:56:47 by hashin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_alphabet(void)
+{
+	char	alphabet;
+
+	alphabet = 'a';
+	while (alphabet <= 'z')
+	{
+		write(1, &alphabet, 1);
+		alphabet++;
+	}
+}
