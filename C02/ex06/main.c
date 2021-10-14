8bd06cbf6e6ad7ec780fe5865889f4bb3457c8d5
Hayeong8957 <judy.hayoung.shin@gmail.com>
2021-10-14 20:30:24 +0900

    ex06

---
+++ C02/ex06/main.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: id <id@student.42seoul.kr>                 +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/14 20:06:28 by id                #+#    #+#             */
+/*   Updated: 2021/10/14 20:28:47 by id               ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int	ft_str_is_printable(char *str);
+
+int	main(void)
+{
+	char *str1;
+	char *str2;
+	char *str3;
+
+	str1 = "Hello~";
+	str2 = "\1F46D";
+	str3 = " ";
+
+	printf("%d\n", ft_str_is_printable(str1));
+	printf("%d\n", ft_str_is_printable(str2));
+	printf("%d\n", ft_str_is_printable(str3));
+}
