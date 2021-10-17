4d3de270c35a04a318708648dca128f735306ee0
Hayeong8957 <judy.hayoung.shin@gmail.com>
2021-10-18 01:21:45 +0900

    ex07

--- C00/ex07/ft_putnbr.c
+++ C00/ex07/ft_putnbr.c
@@ -6,7 +6,7 @@
 /*   By: hashin <hashin@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/11 15:21:12 by hashin            #+#    #+#             */
-/*   Updated: 2021/10/11 17:03:13 by hashin           ###   ########.fr       */
+/*   Updated: 2021/10/18 01:21:19 by id               ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,20 +18,20 @@ void	ft_putnbr(int nb)
 {
 	if (nb == -2147483648)
 	{
-		write(1, &nb, 11);
+		write(1, "-2147483648", 11);
 		return ;
-	}
-	else if (nb > 9)
-	{
-		ft_putnbr(nb / 10);
-		ft_putnbr(nb % 10);
 	}
 	else if (nb < 0)
 	{
 		ft_putchar('-');
 		nb = -nb;
 	}
-	else
+	if (nb > 9)
+	{
+		ft_putnbr(nb / 10);
+		ft_putnbr(nb % 10);
+	}
+	if (nb <= 9)
 	{
 		ft_putchar(nb + 48);
 	}
