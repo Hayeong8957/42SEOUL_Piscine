da20ffe17eb0fa01f7932189d77cf8ecbb108a20
Hayeong8957 <judy.hayoung.shin@gmail.com>
2021-10-21 12:06:17 +0900

    ex03

--- C04/ex03/ft_atoi.c
+++ C04/ex03/ft_atoi.c
@@ -1,49 +1,36 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*   ft_atoi2.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hashin <hashin@student.42seoul.kr>         +#+  +:+       +#+        */
+/*   By: id <id@student.42seoul.kr>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/10/20 10:45:28 by hashin            #+#    #+#             */
-/*   Updated: 2021/10/20 13:58:57 by hashin           ###   ########.fr       */
+/*   Created: 2021/10/21 11:58:16 by id                #+#    #+#             */
+/*   Updated: 2021/10/21 12:04:48 by id               ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_whitespace(char *str)
-{
-	int	i;
-
-	i = 0;
-	while ((str[i] >= 9 && str[i] <= 13) || (str[i] == 32))
-		i++;
-	return (i);
-}
-
-int	ft_isdigit(int c)
-{
-	if (c >= '0' && c <= '9')
-		return (1);
-	return (0);
-}
-
 int	ft_atoi(char *str)
 {
-	int	check;
 	int	i;
-	int	num;
+	int	check;
+	int	result;
 
+	i = 0;
 	check = 1;
-	i = ft_whitespace(str);
-	num = 0;
-	while (str[i++] == '-' || str[i++] == '+')
+	result = 0;
+	while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
+		i++;
+	while (str[i] == '+' || str[i] == '-')
 	{
 		if (str[i] == '-')
-			check = -1;
-		else
-			check = 1;
+			check *= -1;
+		i++;
 	}
-	while (ft_isdigit(str[i]) == 1)
-		num = (num * 10) + (str[i++] - '0');
-	return (check * num);
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		result = (str[i] - '0') + (result * 10);
+		i++;
+	}
+	return (result * check);
 }
