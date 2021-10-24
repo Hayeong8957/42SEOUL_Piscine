f53162e12e4fe8c9431f98ab8f30c38715f96777
Hayeong8957 <judy.hayoung.shin@gmail.com>
2021-10-25 04:47:35 +0900

    ex03

---
+++ C06/ex03/ft_sort_params.c
@@ -0,0 +1,76 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_params.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: id <id@student.42seoul.kr>                 +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/25 04:25:33 by id                #+#    #+#             */
+/*   Updated: 2021/10/25 04:46:22 by id               ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	int	i;
+	int	result;
+
+	i = 0;
+	while (s1[i] != '\0' || s2[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+		{
+			result = s1[i] - s2[i];
+			return (result);
+		}
+		i++;
+	}
+	return (0);
+}
+
+void	print_arg(int argc, char **argv)
+{
+	int	i;
+	int	j;
+
+	i = 1;
+	while (i < argc)
+	{
+		j = 0;
+		while (argv[i][j] != '\0')
+		{
+			ft_putchar(argv[i][j]);
+			j++;
+		}
+		ft_putchar('\n');
+		i++;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	int	i;
+	char	*temp;
+
+	i = 0;
+	while (i + 1 < argc)
+	{
+		if (ft_strcmp(argv[i + 1], argv[i]) < 0)
+		{
+			temp = argv[i];
+			argv[i] = argv[i + 1];
+			argv[i + 1] = temp;
+			i = 0;
+		}
+		i++;
+	}
+	print_arg(argc, argv);
+	return (0);
+}
