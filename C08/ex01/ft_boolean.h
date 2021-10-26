d3d9fed73496faaae44d30c499b48c606dae6142
Hayeong8957 <judy.hayoung.shin@gmail.com>
2021-10-26 21:08:15 +0900

    ex01

--- C08/ex01/ft_boolean.h
+++ C08/ex01/ft_boolean.h
@@ -6,7 +6,7 @@
 /*   By: id <id@student.42seoul.kr>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/26 16:33:06 by id                #+#    #+#             */
-/*   Updated: 2021/10/26 20:34:36 by id               ###   ########.fr       */
+/*   Updated: 2021/10/26 21:00:53 by id               ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,16 @@
 
 # include <unistd.h>
 
-typedef	int	t_bool;
+typedef int	t_bool;
 
 # define EVEN(number) (number % 2 == 0)
 
 # define TRUE 1
+
 # define FALSE 0
 
 # define EVEN_MSG "I have an even number of arguments.\n"
+
 # define ODD_MSG "I have an odd number of arguments.\n"
 
 # define SUCCESS 0
